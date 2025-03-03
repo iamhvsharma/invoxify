641111944f4a4f9edda81e92b5cfa216a4ff91a8
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-03-04 00:13:56 +0530

    Login page UI done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Login() {
-    return (
-        <div>
-            <h1>Hello from Login Route</h1>
-        </div>
-    )
+  return (
+    <>
+      <div className="flex h-screen w-full items-center justify-center px-4">
+        <Card className="max-w-sm">
+          <CardHeader className="flex gap-5">
+            <CardTitle className="text-2xl flex justify-center">
+              Login
+            </CardTitle>
+            <CardDescription>
+              Welcome to Invoxify, Enter email below to Login
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className="flex flex-col gap-y-4">
+              <div className="flex flex-col gap-y-2  ">
+                <Label> Email </Label>
+                <Input placeholder="crazyharsh@invoxify.com" />
+              </div>
+              <Button className="text-white">Submit</Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 }
