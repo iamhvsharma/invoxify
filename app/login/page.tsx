18d7bfacaf9c695e355f9b06951d706d319d658f
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-03-07 01:46:06 +0530

    login page completed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,8 +9,16 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signIn } from "../utils/auth";
+import { SubmitButton } from "@/components/Custom/SubmitButton";
+import { requireUser } from "../utils/hooks";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await requireUser();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <>
       <div className="flex h-screen w-full items-center justify-center px-4">
@@ -24,15 +32,23 @@ export default function Login() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form action={async () => {
-              "use server";
-              await signIn();
-            }} className="flex flex-col gap-y-4">
+            <form
+              action={async (formData) => {
+                "use server";
+                await signIn("nodemailer", formData);
+              }}
+              className="flex flex-col gap-y-4"
+            >
               <div className="flex flex-col gap-y-2  ">
                 <Label> Email </Label>
-                <Input placeholder="crazyharsh@invoxify.com" />
+                <Input
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="crazyharsh@invoxify.com"
+                />
               </div>
-              <Button className="text-white">Submit</Button>
+              <SubmitButton />
             </form>
           </CardContent>
         </Card>
