0bcf3d0753ceeaa51255556c832457a06db9d10a
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-03-08 15:05:06 +0530

    Made the submit button dynamic

--- components/Custom/SubmitButton.tsx
+++ components/Custom/SubmitButton.tsx
@@ -6,7 +6,12 @@ import {Loader2} from "lucide-react"
 
 // UseFormStatus relies on the javascript bundle hence we used "use client" that means this component will run on client side
 
-export function SubmitButton(){
+// Making this component Dynamic
+interface iAppProps {
+    text: string;
+}
+
+export function SubmitButton({ text } : iAppProps){
     const { pending } = useFormStatus();
 
     return (
@@ -15,7 +20,7 @@ export function SubmitButton(){
             <Loader2 className="size-4 mr-2 animate-spin"/> Please wait...
             </Button>) : (
                 <Button type="submit" className="w-full">
-                    Submit
+                    {text}
                 </Button>
             )}
         </>
